6086cae1dd12806eed380aeec4e04258133f213a
iqbaltaif88 <iqbaltaif88@gmail.com>
2026-08-23 14:50:04 +0600

    postgresql connection added

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # SQL_DATABASE_URL = 'mysql+pymysql://root:4321@127.0.0.1:3306/todoapplicationdatabase'
 # SQL_DATABASE_URL = 'postgresql://postgres:4321@localhost/todoAplicationDatabase'
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.btjgyygzcetvmyjoprws:XUFORIGEUtLuiNDY@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 # engine = create_engine(SQL_DATABASE_URL)
